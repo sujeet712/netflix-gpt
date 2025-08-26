58598f6ad7f746756f001df78180570a874be0a8
Sujeet <sujeet.iiitm@.com>
2025-08-26 16:24:31 +0530

    deployment done

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,15 +7,37 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAFT5NALX6BGeA8SLtCAofcXC8HkRhBuCk",
-  authDomain: "netflix-gpt-17cd7.firebaseapp.com",
-  projectId: "netflix-gpt-17cd7",
-  storageBucket: "netflix-gpt-17cd7.firebasestorage.app",
-  messagingSenderId: "410129256557",
-  appId: "1:410129256557:web:f240fe6b1bc032deb56f64",
-  measurementId: "G-7KDCF918PM",
+  apiKey: "AIzaSyA5sytFy4LdijCSE0IEMaTBtlOoMGRPHFY",
+  authDomain: "netflixgpt-508e9.firebaseapp.com",
+  projectId: "netflixgpt-508e9",
+  storageBucket: "netflixgpt-508e9.firebasestorage.app",
+  messagingSenderId: "132309437562",
+  appId: "1:132309437562:web:ead98cb0a4b30889cf7fc3",
+  measurementId: "G-V8PG3HSN92",
 };
 
-// Initialize Firebase
+// Initialize Firebase 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAFT5NALX6BGeA8SLtCAofcXC8HkRhBuCk",
+//   authDomain: "netflix-gpt-17cd7.firebaseapp.com",
+//   projectId: "netflix-gpt-17cd7",
+//   storageBucket: "netflix-gpt-17cd7.firebasestorage.app",
+//   messagingSenderId: "410129256557",
+//   appId: "1:410129256557:web:f240fe6b1bc032deb56f64",
+//   measurementId: "G-7KDCF918PM",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
